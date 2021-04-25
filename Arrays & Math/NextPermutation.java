238da11d3fa9411cc0051f59631e30393f124444
alikgarai <garaialik247@gmail.com>
2021-04-25 16:29:17 +0530

    Create NextPermutation.java

---
+++ Arrays & Math/NextPermutation.java
@@ -0,0 +1,53 @@
+// Problem: https://www.scaler.com/academy/mentee-dashboard/classroom/array-and-math-0eedd85d-7959-45c3-a09b-77ef76de3b6f/#homework
+
+public class NextPermutation {
+
+	public int[] nextPermutation(int[] A) {
+
+		int l = A.length;
+		int k, tmp;
+
+		for (k = l - 2; k >= 0; k--) {
+			if (A[k] < A[k + 1]) {
+				break;
+			}
+		}
+
+		if (k == -1) {
+			Arrays.sort(A);
+			return A;
+		}
+
+		for (int i = l - 1; i >= 0; i--) {
+			if (A[i] > A[k]) {
+				tmp = A[i];
+				A[i] = A[k];
+				A[k] = tmp;
+				break;
+			}
+		}
+
+		Arrays.sort(A, k + 1, l);
+
+		return A;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n, A[];
+
+		n = s.nextInt();
+		A = new int[n];
+		for (int i = 0; i < n; i++) {
+			A[i] = s.nextInt();
+		}
+
+		NextPermutation c = new NextPermutation();
+		A = c.nextPermutation(A);
+		for (int i = 0; i < n; i++) {
+			System.out.print(A[i] + " ");
+		}
+		s.close();
+	}
+}
