5f264664dd3144aeccc86cf51b7d18309a23f877
alikgarai <garaialik247@gmail.com>
2021-04-25 17:14:38 +0530

    Update MaxAbsoluteDifference.java

--- Arrays & Math/Solutions/MaxAbsoluteDifference.java
+++ Arrays & Math/Solutions/MaxAbsoluteDifference.java
@@ -1,5 +1,3 @@
-// Problem: https://www.scaler.com/academy/mentee-dashboard/classroom/array-and-math-0eedd85d-7959-45c3-a09b-77ef76de3b6f/#assignment
-
 public class MaximumAbsoluteDifference {
 
 	public int maxArr(int[] A) {
